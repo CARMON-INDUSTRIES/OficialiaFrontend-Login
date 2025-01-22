2df577590f78ddaf7c18b897aba3101dc4ba725e
DESKTOP-5ULSK89\CAROL <carolmonroy54@gmail.com>
2025-01-22 12:49:33 -0600

    Agregar boton registro

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -12,16 +12,25 @@ const LandingPage = () => {
     >
       <div className="text-center bg-white bg-opacity-70 rounded-lg p-8 shadow-lg">
         <h1 className="text-5xl font-bold mb-6 text-[#691B31]">
-          Unidad Central de Correspondencia <br /> Tula de Allende
+          Unidad Central de Correspondencia 
         </h1>
+        <h2 className="text-4xl font-bold mb-6 text-[#691B31]">
+          Tula de Allende
+        </h2>
         <p className="text-lg text-[#333] mb-8">
           Selecciona una opción para continuar:
         </p>
         <div className="flex flex-col sm:flex-row gap-6 justify-center">
           {/* Botón para inicio de sesión */}
           <Link href="/login">
-            <button className="bg-[#691B31] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#8A233E] transition-all">
+            <button className="bg-[#691B31] text-white px-7 py-3 rounded-lg shadow-md hover:bg-[#8A233E] transition-all">
               Iniciar Sesión
+            </button>
+          </Link>
+          {/* Botón para inicio de sesión */}
+          <Link href="/register">
+            <button className="bg-[#BC995B] text-white px-7 py-3 rounded-lg shadow-md hover:bg-[#A37F4D] transition-all">
+              Registrate
             </button>
           </Link>
         </div>
