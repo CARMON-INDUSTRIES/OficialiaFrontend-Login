eddae8f2b75725e345a0210e1840e4c6d27f4fbf
DESKTOP-B36C4MV\Ramon <ganjahh420.bh@gmail.com>
2025-02-06 12:37:30 -0600

    apuntando al nuevo endpoint obtener-comunidades

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -26,7 +26,7 @@ const Formulario = () => {
     const fetchComunidades = async () => {
       try {
         const response = await axios.get(
-          "https://oficialialoginbackend.somee.com/api/Correspondencia/obtener",
+          "https://oficialialoginbackend.somee.com/api/Correspondencia/obtener-comunidades",
           {
             headers: { Authorization: `Bearer ${token}` },
           }
