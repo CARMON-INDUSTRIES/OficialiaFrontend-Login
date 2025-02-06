69545da6bb6b9c261e7684d133cfee3554340bd9
DESKTOP-B36C4MV\Ramon <ganjahh420.bh@gmail.com>
2025-02-06 11:10:45 -0600

    /Correspondencia/obtener

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -26,7 +26,7 @@ const Formulario = () => {
     const fetchComunidades = async () => {
       try {
         const response = await axios.get(
-          "https://oficialialoginbackend.somee.com/api/Comunidades/obtener",
+          "https://oficialialoginbackend.somee.com/api/Correspondencia/obtener",
           {
             headers: { Authorization: `Bearer ${token}` },
           }
