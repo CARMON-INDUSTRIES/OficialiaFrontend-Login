ebf276b33f71983bc6fd9a5c2ddc20514225a8a3
DESKTOP-B36C4MV\Ramon <ganjahh420.bh@gmail.com>
2025-02-13 10:53:53 -0600

    prueba celular

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -294,7 +294,7 @@ const Formulario = () => {
             </select>
           </div>
           <div>
-          <label className="block font-bold">Archivo</label>
+          <label className="block font-bold">Subir Archivo</label>
             <UploadButton
               route="demo"
               accept="application/pdf"
