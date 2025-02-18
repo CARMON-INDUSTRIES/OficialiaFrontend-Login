93b314a78028e27c469c3f1cc8eda87bb256d444
DESKTOP-5ULSK89\CAROL <carolmonroy54@gmail.com>
2025-02-18 08:29:46 -0600

    no message

---
+++ src/app/buzon/page.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import Buzon from "@/components/Buzon";
+
+const BuzonPage = () => {
+  return (
+    <Layout>
+      <Buzon />
+    </Layout>
+  );
+};
+
+export default BuzonPage;
