284d9d69ed8f57ce9535bd1d0ebd85a1058176ea
DESKTOP-5ULSK89\CAROL <carolmonroy54@gmail.com>
2025-02-19 14:32:32 -0600

    formulario de nuevo REGISTRO USUARIO

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import {
-  FaEye,
-  FaEyeSlash
-} from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
@@ -31,7 +28,7 @@ export const Register = () => {
       );
 
       if (!response.ok) throw new Error("Error al registrar usuario");
-      
+
       const data = await response.json();
       setError("");
 
@@ -42,12 +39,10 @@ export const Register = () => {
         icon: "success",
         confirmButtonColor: "#691B31",
         confirmButtonText: "Aceptar",
-  
       });
-
     } catch (err) {
       setError(err.message);
-      
+
       // SweetAlert de error
       Swal.fire({
         title: "¡Error!",
@@ -60,54 +55,85 @@ export const Register = () => {
   };
 
   return (
-      <div className="right flex flex-col justify-center items-center relative">
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className="lg:w-[26.5rem] w-80 flex flex-col gap-4"
+    <div className="right flex flex-col justify-center items-center relative">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="lg:w-[26.5rem] w-80 flex flex-col gap-4"
+      >
+        <div className="text-center sm:text-left">
+          <h1 className="text-center text-3xl font-bold text-[#621132] ">
+            Nuevo Usuario
+          </h1>
+          <br />
+          <img
+            src="/images/presidencia.jpeg"
+            alt="Presidencia"
+            className="w-36 h-auto object-cover mb-4 mx-auto"
+          />
+        </div>
+
+        <form
+          onSubmit={handleRegister}
+          className="bg-white rounded-[20px] p-8 flex flex-col gap-5 lg:border-none border border-[#e6ebf4]"
         >
-          <div className="text-center sm:text-left">
-            <h1 className="text-center text-3xl font-bold text-[#621132] ">Nuevo Usuario</h1>
-            <br />
-            <img src="/images/presidencia.jpeg" alt="Presidencia" className="w-36 h-auto object-cover mb-4 mx-auto" />
+          <div className="name flex flex-col gap-3 items-start">
+            <label className="text-sm">Nombre de usuario</label>
+            <input
+              type="text"
+              placeholder="Ingrese un nombre sin espacios"
+              className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5]"
+              value={userName}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
 
-          <form
-            onSubmit={handleRegister}
-            className="bg-white rounded-[20px] p-8 flex flex-col gap-5 lg:border-none border border-[#e6ebf4]"
+          <div className="email flex flex-col gap-3 items-start">
+            <label className="text-sm">Correo Electrónico</label>
+            <input
+              type="email"
+              placeholder="Ingrese su correo"
+              className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5]"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="password flex flex-col gap-3 items-start relative">
+            <label className="text-sm">Contraseña</label>
+            <div className="relative w-full">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Minimo 6 caracteres"
+                className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5] pr-10"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#691B31]"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+
+          
+
+          {error && <p className="text-red-500">{error}</p>}
+
+          <button
+            type="submit"
+            className="bg-[#BC995B] text-white rounded-[10px] py-2"
           >
-            <div className="name flex flex-col gap-3 items-start">
-              <label className="text-sm">Nombre de usuario</label>
-              <input type="text" placeholder="Ingrese un nombre sin espacios" className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5]" value={userName} onChange={(e) => setName(e.target.value)} />
-            </div>
-
-            <div className="email flex flex-col gap-3 items-start">
-              <label className="text-sm">Correo Electrónico</label>
-              <input type="email" placeholder="Ingrese su correo" className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5]" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-
-            <div className="password flex flex-col gap-3 items-start relative">
-              <label className="text-sm">Contraseña</label>
-              <div className="relative w-full">
-                <input type={showPassword ? "text" : "password"} placeholder="Minimo 6 caracteres" className="w-full px-4 py-2 rounded-lg border-none outline-none bg-[#F5F5F5] pr-10" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#691B31]">
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </button>
-              </div>
-            </div>
-
-            {error && <p className="text-red-500">{error}</p>}
-
-            <button type="submit" className="bg-[#BC995B] text-white rounded-[10px] py-2">Registrarse</button>
-
-            
-          </form>
-        </motion.div>
-      </div>
-    
+            Registrarse
+          </button>
+        </form>
+      </motion.div>
+    </div>
   );
 };
 
 export default Register;
-
